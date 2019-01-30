24fa88e9e8629db811fcc36be09584a165ae0f87
Leslie Shea <leslieyshea@gmail.com>
2019-01-30 11:32:45 -0800

    started writing first test for random number being less than 0.5 to produce cat

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,15 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function catOrDog(randomNumber) {
+    if(randomNumber < 0.5) {
+        return 'cat';
+    }
+}
+
+test ('return cat if random number is less than 0.5', function(assert) {
+    const randomNumber = 0.4;
+    const result = catOrDog(randomNumber);
+    const expected = 'cat';
+
+    assert.equal(result, expected);
 });
